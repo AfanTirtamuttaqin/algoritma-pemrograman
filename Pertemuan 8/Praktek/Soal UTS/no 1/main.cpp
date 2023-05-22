e1de3594313bd1142e4c9738bc0b16217c86a804
AfanTirtamuttaqin <127401335+AfanTirtamuttaqin@users.noreply.github.com>
2023-05-22 22:53:41 +0700

    Add files via upload

---
+++ Pertemuan 8/Praktek/Soal UTS/no 1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+bool ganjil(int a){
+    return a%2==1? 1:0;
+}
+void mingenap(int ar[],int n){
+    int temp=ar[0];
+    int index;
+    for(int i=0;i<n;i++){
+        if(!ganjil(ar[i])){
+            if(ar[i]<temp){
+                temp=ar[i];
+                index=i;
+            }
+        }
+    }
+    cout<<"Nilai genap terkecil = "<<temp<<" pada index ke-"<<index;
+}
+int main()
+{
+    int ar[5]={99,4,6,2,97};
+    int n=5;
+    mingenap(ar,n);
+    return 0;
+}
